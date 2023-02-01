4b8f1058a23d38be123d75f40bb42ae0867e3895
Norman Eshuchi <normaneshuchi@gmail.com>
2023-02-01 09:29:46 +0300

    Added mantine and react query example

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -7,22 +7,19 @@ type Props = {
     children: ReactNode
 }
 
-const queryClient = new QueryClient()
 
 export default function MainLayout({ children }: Props) {
     return (
-        <QueryClientProvider client={queryClient}>
-            <AppShell
-                padding="md"
-                navbar={<Navbar width={{ base: 300 }} height={'100vh'} p="xs"><Text>Side nav content goes here</Text></Navbar>}
-                header={<Header height={60} p="xs"><Text>Top nav content goes here</Text></Header>}
-                styles={(theme) => ({
-                    main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-                })}
-            >
-                {children}
-            </AppShell>
-        </QueryClientProvider>
+        <AppShell
+            padding="md"
+            navbar={<Navbar width={{ base: 300 }} height={'100vh'} p="xs"><Text>Side nav content goes here</Text></Navbar>}
+            header={<Header height={60} p="xs"><Text>Top nav content goes here</Text></Header>}
+            styles={(theme) => ({
+                main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+            })}
+        >
+            {children}
+        </AppShell>
 
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,23 @@ import App from './App'
 import './index.css'
 import { router } from './routes/routes'
 import { MantineProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MantineProvider
-      theme={{
-        fontFamily: 'Inter, Avenir, Helvetica, Arial, sans-serif',
-      }}
-      withGlobalStyles withNormalizeCSS>
+    <QueryClientProvider client={queryClient}>
 
-      <RouterProvider router={router} />
-    </MantineProvider>
+      <MantineProvider
+        theme={{
+          fontFamily: 'Inter, Avenir, Helvetica, Arial, sans-serif',
+        }}
+        withGlobalStyles withNormalizeCSS>
+
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
